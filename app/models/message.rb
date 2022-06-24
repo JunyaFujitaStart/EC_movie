52576be94f6a4e0d2e7949ee1913a66b0dbfdc39
j.f <usausa13108891@gmail.com>
2022-06-24 23:16:47 +0000

    portfolio EC_movie

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,21 +1,3 @@
 class Message < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
-  
-  def self.xxxx
-    File.read("#{Rails.root}/app/models/text.txt").chomp
-  end
-  
-  def self.yyyy
-    file = File.open("#{Rails.root}/app/models/write.txt", "w")
-    file.puts("Hello, You can write your message.")
-    file.close
-  end
-  
-  def self.zzzz
-    File.read("#{Rails.root}/app/models/write.txt").chomp
-  end
-  
-  def self.read
-    File.read("#{Rails.root}/app/controllers/img_url.txt")
-  end
 end
