80c59a11ddaaa463328dcec9df3ac78c4c6f9b96
j.f <usausa13108891@gmail.com>
2022-06-26 09:30:06 +0000

    modal and link 1

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -55,3 +55,27 @@ $(function() {
       ]
     });
 });			
+
+const buttonOpen = document.getElementById('modalOpen');
+const modal = document.getElementById('easyModal');
+const buttonClose = document.getElementsByClassName('modalClose')[0];
+
+// ボタンがクリックされた時
+buttonOpen.addEventListener('click', modalOpen);
+function modalOpen() {
+  modal.style.display = 'block';
+}
+
+// バツ印がクリックされた時
+buttonClose.addEventListener('click', modalClose);
+function modalClose() {
+  modal.style.display = 'none';
+}
+
+// モーダルコンテンツ以外がクリックされた時
+addEventListener('click', outsideClose);
+function outsideClose(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
